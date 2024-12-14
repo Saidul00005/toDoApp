203693559677b65823dad79940d354afb2ebf61d
Mohammad Sayadul Hoque <Saidul1000005@gmail.com>
2024-12-14 16:52:07 +0600

    Some fix oriented changes in todo update modal and add new todo form.

--- components/addNewItem/addNewItemForm.js
+++ components/addNewItem/addNewItemForm.js
@@ -143,21 +143,17 @@ const AddNewItemForm = () => {
 
           {/* Deadline Date and Time */}
           <div className="flex flex-col">
-            {/* <label
+            <label
               htmlFor="toDoACT"
               className="block text-xs font-medium mb-2 text-gray-800 dark:text-gray-200"
             >
               Assumptive Completion Time:<span className="text-red-500"> *</span>
-            </label> */}
-            <Input
+            </label>
+            <input
               id="toDoACT"
               type="datetime-local"
-              isRequired
-              variant='bordered'
-              label='Assumptive Completion Time:'
-              labelPlacement='outside'
-              placeholder='a'
-              size='sm'
+              required
+              className="w-full px-4 py-2 text-sm border border-gray-300 dark:border-gray-600 rounded-md shadow-sm bg-gray-50 dark:bg-gray-700 text-gray-900 dark:text-gray-200 focus:ring-2 focus:ring-blue-500 focus:border-blue-500 dark:focus:ring-blue-400 dark:focus:border-blue-400"
               {...register("toDoACT", {
                 required: "Completion time is required",
                 validate: {
--- components/toDoList/toDoEdit.js
+++ components/toDoList/toDoEdit.js
@@ -187,8 +187,15 @@ const ToDoEdit = ({ id, status }) => {
               }}
               render={({ field }) => (
                 <div>
-                  <Input {...field} type="datetime-local" isRequired variant='bordered' id='toDoACT' label='Assumptive Completion Time:' labelPlacement='outside'
-                    placeholder='a' size='sm' />
+                  <label
+                    htmlFor="toDoACT"
+                    className="block text-xs font-medium mb-2 text-gray-800 dark:text-gray-200"
+                  >
+                    Assumptive Completion Time:<span className="text-red-500"> *</span>
+                  </label>
+                  <input {...field} id="toDoACT" type="datetime-local" required
+                    className="w-full px-4 py-2 text-sm border border-gray-300 dark:border-gray-600 rounded-md shadow-sm bg-gray-50 dark:bg-gray-700 text-gray-900 dark:text-gray-200 focus:ring-2 focus:ring-blue-500 focus:border-blue-500 dark:focus:ring-blue-400 dark:focus:border-blue-400"
+                  />
                   <ErrorMessage
                     errors={errors}
                     name="toDoACT"
