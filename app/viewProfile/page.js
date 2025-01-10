b7a4910dab9647031c2c9a9e3daf3039be6842f1
Mohammad Sayadul Hoque <Saidul1000005@gmail.com>
2025-01-10 19:39:18 +0600

    Update 6 .

--- app/viewProfile/page.js
+++ app/viewProfile/page.js
@@ -1,12 +1,11 @@
 'use client'
 
 import { useDispatch, useSelector } from 'react-redux'
-import { selectUserProfile, selectUserProfileLoading, selectUserProfileError, fetchUserProfile, updateUserProfile } from '@/app/redux/slices/userProfileSlice'
+import { selectUserProfile, selectUserProfileLoading, selectUserProfileError, fetchUserProfile } from '@/app/redux/slices/userProfileSlice'
 import { useSession, signOut } from "next-auth/react"
-import { useEffect } from 'react'
-import { Card, CardBody, CardHeader, Button, Input } from "@nextui-org/react"
-import { useForm, Controller } from "react-hook-form"
-import { useToast } from '@/components/toastMessage/toastContext';
+import { useEffect, useState } from 'react'
+import { Card, CardBody, CardHeader, Button } from "@nextui-org/react"
+import UpdateProfileForm from '@/components/viewProfile/UpdateProfileForm'
 
 const Loading = () => (
   <div className="flex flex-col items-center justify-center h-[50vh]">
@@ -30,46 +29,14 @@ const Page = () => {
   const profile = useSelector(selectUserProfile)
   const loading = useSelector(selectUserProfileLoading)
   const error = useSelector(selectUserProfileError)
-  const { showToast } = useToast();
 
-  const { control, handleSubmit, setValue, watch, isSubmitting } = useForm({
-    defaultValues: {
-      name: '',
-      city: '',
-      country: '',
-      password: ''
-    }
-  })
-
-  const isEditing = watch('isEditing')
+  const [isEditing, setIsEditing] = useState(false);
 
   useEffect(() => {
     if (session && !profile) {
       dispatch(fetchUserProfile())
     }
   }, [session, dispatch, profile])
-
-  useEffect(() => {
-    if (profile) {
-      setValue('name', profile.name || '')
-      setValue('city', profile.city || '')
-      setValue('country', profile.country || '')
-    }
-  }, [profile, setValue])
-
-  const onSubmit = (data) => {
-    const { isEditing, ...updatedData } = data;
-    const updatedUserProfile = { ...updatedData };
-    dispatch(updateUserProfile(updatedUserProfile))
-    setValue('isEditing', false)
-    setValue('password', '')
-    showToast("Profile Updated", 'success')
-  }
-
-  const handleCancel = () => {
-    setValue('isEditing', false)
-    setValue('password', '') // Reset password field
-  }
 
   const handleSignOut = () => {
     signOut({ callbackUrl: '/' });
@@ -91,7 +58,7 @@ const Page = () => {
           <h1 className="text-2xl font-bold">User Profile</h1>
           {!isEditing && (
             <div className='flex gap-2'>
-              <Button color="primary" variant='flat' size='md' onPress={() => setValue('isEditing', true)}>
+              <Button color="primary" variant='flat' size='md' onPress={() => setIsEditing(true)}>
                 Update Profile
               </Button>
               <Button color="danger" variant='flat' size='md' onPress={handleSignOut}>
@@ -109,68 +76,7 @@ const Page = () => {
               <p><strong>Country: </strong>{profile.country}</p>
             </div>
           ) : (
-            <form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
-              <Controller
-                name="name"
-                control={control}
-                rules={{ required: "Name is required" }}
-                render={({ field, fieldState: { error } }) => (
-                  <Input
-                    {...field}
-                    label="Name"
-                    errorMessage={error?.message}
-                    isInvalid={!!error}
-                  />
-                )}
-              />
-              <Controller
-                name="city"
-                control={control}
-                rules={{ required: "City is required" }}
-                render={({ field, fieldState: { error } }) => (
-                  <Input
-                    {...field}
-                    label="City"
-                    errorMessage={error?.message}
-                    isInvalid={!!error}
-                  />
-                )}
-              />
-              <Controller
-                name="country"
-                control={control}
-                render={({ field, fieldState: { error } }) => (
-                  <Input
-                    {...field}
-                    label="Country"
-                    errorMessage={error?.message}
-                    isInvalid={!!error}
-                  />
-                )}
-              />
-              <Controller
-                name="password"
-                control={control}
-                rules={{ required: "Password is required" }}
-                render={({ field, fieldState: { error } }) => (
-                  <Input
-                    {...field}
-                    type="password"
-                    label="Password"
-                    errorMessage={error?.message}
-                    isInvalid={!!error}
-                  />
-                )}
-              />
-              <div className="flex justify-end space-x-2">
-                <Button color="danger" onPress={handleCancel}>
-                  Cancel
-                </Button>
-                <Button color="primary" type="submit" isLoading={isSubmitting}>
-                  Save Changes
-                </Button>
-              </div>
-            </form>
+            <UpdateProfileForm profile={profile} setIsEditing={setIsEditing} />
           )}
         </CardBody>
       </Card>
